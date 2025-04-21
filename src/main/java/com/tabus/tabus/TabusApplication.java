d74643f49b1d6a47c56497f6ab6ad5639bb89665
hyt <15037944+hyt1717@user.noreply.gitee.com>
2025-04-21 15:04:14 +0800

    初始框架

---
+++ src/main/java/com/tabus/tabus/TabusApplication.java
@@ -0,0 +1,15 @@
+package com.tabus.tabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TabusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tabus/tabus/TabusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabus.tabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
